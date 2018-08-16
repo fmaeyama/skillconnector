6da63efd1afb59372c1d5946267b2cfb08807e2c
fmaeyama <bariolage@live.com>
2018-08-16 21:46:39 +0900

    i18nの解決
    office.edit整形済み

--- var/www/html/skillconnect/app/controllers/home_controller.rb
+++ var/www/html/skillconnect/app/controllers/home_controller.rb
@@ -1,15 +1,5 @@
 class HomeController < ApplicationController
 
-  
-  def index
-    # TODO: 今日のお知らせを表示（期限を切って表示させる）
-    # TODO: 権限に応じて、sysadmin, bizadminのメニューを表示（Expanded)
-    @visibleSysadmin = true;
-    @visiblebizadmin = true;
-    @page_title = "SILVERION menu"
-    @title = '総合メニュー'
-  end
-
   def sysadmin
     # システム管理者メニュー
     @link = {
@@ -20,16 +10,16 @@ class HomeController < ApplicationController
         '権限管理'=>[
             {title: 'ユーザー権限割当', controller: 'common_users', action: 'assign_role'},
             {title: '権限設定画面', controller: 'privilege', action: 'assign_role'}],
-        t(:cmn_dict.business)+'管理'=>[
-            {title: t(:cmn_dict.business)+'一覧',controller:"businesses", action:"index"},
-            [title: t(:cmn_dict.business)+'新規作成',controller:"businesses", action:"new"]],
-        t(:cmn_dict.property)+'管理'=>[
-            {title: t(:cmn_dict.property)+'一覧', controller:"properties", action:"index"},
-            {title: t(:cmn_dict.property)+'新規作成', controller:"properties", action:"new"}
+        t(:business, scope:[:cmn_dict])+'管理'=>[
+            {title: t(:business, scope:[:cmn_dict])+'一覧',controller:"businesses", action:"index"},
+            [title: t(:business, scope:[:cmn_dict])+'新規作成',controller:"businesses", action:"new"]],
+        t(:property, scope:[:cmn_dict])+'管理'=>[
+            {title: t(:property, scope:[:cmn_dict])+'一覧', controller:"properties", action:"index"},
+            {title: t(:property, scope:[:cmn_dict])+'新規作成', controller:"properties", action:"new"}
         ],
-        t(:cmn_dict.project)+'管理'=>[
-            {title: t(:cmn_dict.project)+'一覧', controller: 'projects', action: "index" },
-            {title: t(:cmn_dict.project)+'新規作成', controller: 'projects', action: "index" }]
+        t(:project, scope:[:cmn_dict])+'管理'=>[
+            {title: t(:project, scope:[:cmn_dict])+'一覧', controller: 'projects', action: "index" },
+            {title: t(:project, scope:[:cmn_dict])+'新規作成', controller: 'projects', action: "index" }]
     }
     # TODO: ユーザー認証により表示させるか、404を表示する
     @visibleSysadmin = true;
@@ -49,14 +39,14 @@ class HomeController < ApplicationController
             { title:"ログインユーザー一覧", controller:"common_users" , action:"biz_index"},
             { title:"ログインユーザー新規作成", controller:"common_users" , action:"biz_useradd"},
             { title:"登録者情報作成・編集", controller:"common_users", action:"biz_index"}],
-        t(:cmn_dict.business)+'管理'=>[
-            {title: t(:cmn_dict.business)+'編集',controller:"businesses", action:"edit_own"},
-            {title: t(:cmn_dict.business)+'連絡先一覧・編集',controller:"businesses", action:"contact_list"}
+        t(:business, scope:[:cmn_dict])+'管理'=>[
+            {title: t(:business, scope:[:cmn_dict])+'編集',controller:"businesses", action:"edit_own"},
+            {title: t(:business, scope:[:cmn_dict])+'連絡先一覧・編集',controller:"businesses", action:"contact_list"}
         ],
-        t(:cmn_dict.project)+'管理'=>[
-            {title: t(:cmn_dict.project)+'一覧', controller: 'projects', action: "index" },
-            {title: t(:cmn_dict.project)+'詳細', controller: 'projects', action: "detail" },
-            {title: t(:cmn_dict.project)+'新規作成', controller: 'projects', action: "index" }]
+        t(:project, scope:[:cmn_dict])+'管理'=>[
+            {title: t("cmn_dict.project")+'一覧', controller: 'projects', action: "index" },
+            {title: t("cmn_dict.project")+'詳細', controller: 'projects', action: "detail" },
+            {title: t(:project,scope:[:cmn_dict])+'新規作成', controller: 'projects', action: "index" }]
 
     }
 
@@ -69,7 +59,7 @@ class HomeController < ApplicationController
     render action: 'index'
   end
 
-  def useradmin
+  def user
     @visibleSysadmin = true;
     @visiblebizadmin = true;
     @currentIndex = 'useradmin'
@@ -82,6 +72,25 @@ class HomeController < ApplicationController
     }
 
     render action: 'index'
+  end
+
+  def index
+    # TODO: 今日のお知らせを表示（期限を切って表示させる）
+    # TODO: 権限に応じて、sysadmin, bizadminのメニューを表示（Expanded)
+    @visibleSysadmin = true;
+    @visiblebizadmin = true;
+    @page_title = "SILVERION menu"
+    @title = '総合メニュー'
+    if @visiblesysadmin then
+      sysadmin
+    end
+
+    if @visiblebizadmin then
+      sysadmin
+    end
+
+    user
+
   end
 
   private
--- var/www/html/skillconnect/app/models/office.rb
+++ var/www/html/skillconnect/app/models/office.rb
@@ -5,6 +5,6 @@ class Office < ApplicationRecord
   belongs_to :primary_address
   belongs_to :primary_contact
   belongs_to :office_status
-  has_many :contacts
+  has_many :contacts, through: :office_contacts
   has_many :businesses
 end
