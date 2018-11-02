6e16ba6538c9d37e53d71433b520e51152c9c9d9
fmaeyama <fmaeyama@users.noreply.github.com>
2018-11-02 17:50:35 +0900

    gridの実装を開始

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( offcanvas.css )
+Rails.application.config.assets.precompile += %w( lib/**.css lib/**.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
