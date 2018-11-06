2bc6b50188d9017020ae865da2b3986f4d83ef64
fmaeyama <fmaeyama@users.noreply.github.com>
2018-11-06 23:50:57 +0900

    SlickGridを使った表示

--- app/decorators/grid_decorator.rb
+++ app/decorators/grid_decorator.rb
@@ -1,8 +1,12 @@
 class GridDecorator < SkillConnectDecorator
-  delegate_all
+    delegate_all
+    attr_accessor :columns, :data, :table_options
 
-  def initialize
-    super
-    self.model_name = I18n.t('cmn_dict.business')
-  end
+
+    def initialize
+        super
+        @columns = Array.new
+        @data = Array.new
+        self.model_name = I18n.t('cmn_dict.business')
+    end
 end
